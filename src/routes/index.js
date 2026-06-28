d6cedc09caec8b9ee231ab134f2c1a92b20d3997
nandaneeeeeeeeee <nandanisewa@gmail.com>
2026-06-28 22:39:34 +0545

    Add chatbot model, routes and controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import authRoutes from './auth.routes.js';
-const
-    router = express.Router();
+import chatbotRoutes from './chatbot.routes.js';
 
-    router.use('/auth', authRoutes)
+const router = express.Router();
+
+router.use('/auth', authRoutes);
+router.use('/chatbot', chatbotRoutes);
+
 export default router;
